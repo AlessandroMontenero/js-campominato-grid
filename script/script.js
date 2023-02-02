97e5d008ab737b7a4b1240ece5d847c29f3c66d7
Alessandro Montenero <alemonte99@icloud.com>
2023-02-02 10:35:32 +0100

    campo minato

---
+++ script/script.js
@@ -0,0 +1,72 @@
+/*****************************
+ * JS-CAMPOMINATO-GRID
+ * @author Alessandro Montenero 
+ * 
+ * @description
+ * L'utente clicca su un bottone che genererà una griglia di gioco quadrata. 
+ * Ogni cella ha un numero progressivo, da 1 a 100. 
+ * Ci saranno quindi 10 caselle per ognuna delle 10 righe. 
+ * Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
+ * ed emetto un messaggio in console con il numero della cella cliccata.
+ * @bonus
+ * Aggiungere una select accanto al bottone di generazione, che fornisca 
+ * una scelta tra tre diversi livelli di difficoltà:
+ * - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, 
+ * divise in 10 caselle per 10 righe;
+ * - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, 
+ * divise in 9 caselle per 9 righe;
+ * - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, 
+ * divise in 7 caselle per 7 righe;
+ *****************************/ 
+
+/* Creo un elemento griglia */
+let gridId = document.getElementById("grid")    
+
+/* Funzione per creare un div classe "box" */
+
+function newBox(content) {
+    let box = document.createElement('div') 
+    box.classList.add("box")
+    gridId.appendChild(box)
+    box.onclick = function(){
+        console.log(this.innerHTML)
+        this.classList.toggle("azure")
+        if (box.innerHTML == '') {
+            box.innerHTML = content            
+        } 
+        else {
+            box.innerHTML = ''
+        }
+    }
+}
+
+
+function multipleBoxes(template) {
+    for (let i = 0; i < template; i++) {
+        newBox(i + 1, i + 1)
+    }
+}
+
+document.getElementById("start").onclick = function createGrid() {
+    gridId.style.cssText = ''
+    gridId.innerHTML = ''
+    let level = document.getElementById("selectLevel").value 
+    if (level == 'easy'){
+        gridId.style.cssText = 'grid-template-columns:repeat(10,1fr);grid-template-rows:repeat(10,1fr);'
+        multipleBoxes(100)
+    }
+    else if (level == 'medium') {
+        gridId.style.cssText = 'grid-template-columns:repeat(9,1fr);grid-template-rows:repeat(9,1fr);'
+        multipleBoxes(81)
+    }
+
+    else if (level == 'hard') {
+        gridId.style.cssText = 'grid-template-columns:repeat(7,1fr);grid-template-rows:repeat(7,1fr);'
+        multipleBoxes(49)
+    }
+}
+
+
+
+
+
